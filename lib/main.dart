09aaa770a09e492e8bd86e3ffe9ffe0d7d3143fa
Bebo <belzeebubc@gmail.com>
2022-06-19 00:09:22 +0300

    init firebase app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gsc/responsive/mobile_screen_layout.dart';
 import 'package:gsc/responsive/responsive_layout_screen.dart';
 import 'package:gsc/responsive/web_screen_layout.dart';
 import 'package:gsc/utils/colors.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
